ed56bcc0c400c1eb6ffee0ec04f9682283a84bb1
Oskar <oskartdragon@gmail.com>
2024-08-15 07:44:20 +0100

    chore: updated hello world

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export type Message = string;
 
 export const helloWorld = (message: Message) => {
   console.log(`Hello, ${message}!`);
+  console.log('I made a change');
 };
 
 helloWorld('Pfida!!!!');
