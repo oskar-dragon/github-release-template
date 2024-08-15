efbb2f9589eb0e1532efa63ce3225850fcdca21e
Oskar <oskartdragon@gmail.com>
2024-08-15 07:46:23 +0100

    feat: removed changes

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ export type Message = string;
 
 export const helloWorld = (message: Message) => {
   console.log(`Hello, ${message}!`);
-  console.log('I made a change');
 };
 
 helloWorld('Pfida!!!!');
